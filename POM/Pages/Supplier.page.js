67635137d1c581568ee3900c507398cf56bbf20f
amir-sam1 <amirsamii401@gmail.com>
2026-01-05 12:29:57 +0200

    update supplier page file

--- POM/Pages/Supplier.page.js
+++ POM/Pages/Supplier.page.js
@@ -7,13 +7,13 @@ class SupplierPage extends Basepage{
         //Locaors
         this.addNewSupllierBtn = page.locator('#AddNewSupplier_Suppliers');
         this.nameInput = page.locator('#Name');
-        this.serviceTypeDropdown = page.locator('//input[@id="Service_Type"]');
-        this.selectServiceTypeOptions = page.locator('//div[@class="styles_tagOption__iIRzL" and contains(.,\'Airports\')]')
-        this.gtoCurrentAmountInput = page.locator('//span[contains(.,\'GTO Current Amount\')]//following::input[1]')
-        this.currencyDropdown = page.locator('//span[contains(.,\'Currency *\')]//following::input[1]')
-        this.selectCurrencyOptions = page.locator('//div[@class="styles_tagOption__iIRzL" and contains(.,\'GBP\')]')
-        this.saveBtn = page.locator('Save_AddNewSuppliers')
-        this.searchInput = page.locator('#Search..')
+        this.serviceTypeDropdown = page.locator('(//div[@class="ant-select-selector"])[1]');
+        this.selectServiceTypeOptions = page.locator('//div[@class="styles_tagOption__iIRzL" and contains(.,\'Airports\')]');
+        this.gtoCurrentAmountInput = page.locator('//span[contains(.,\'GTO Current Amount\')]//following::input[1]');
+        this.currencyDropdown = page.locator('(//div[@class="ant-select-selector"])[2]');
+        this.selectCurrencyOptions = page.locator('//div[@class="styles_tagOption__iIRzL" and contains(.,\'GBP\')]');
+        this.saveBtn = page.locator('#Save_AddNewSuppliers');
+        this.searchInput = page.locator('[id="Search.."]');
 
 
 
@@ -31,17 +31,17 @@ class SupplierPage extends Basepage{
         await this.nameInput.fill(name);
         await this.serviceTypeDropdown.click();
         await this.selectServiceTypeOptions.click();
-        // await this.gtoCurrentAmountInput.fill('1000');
-        // await this.currencyDropdown.click();
-        // await this.selectCurrencyOptions.click();
-        // await this.saveBtn.click();
+        await this.gtoCurrentAmountInput.fill('1000');
+        await this.currencyDropdown.click();
+        await this.selectCurrencyOptions.click();
+        await this.saveBtn.click();
 
     }
 
     async verifySupplierAddSuccessfullty(name){
         await this.searchInput.fill(name);
         const locator = await this.page.locator(`text=${name}`)
-        expect(locator).toBeVisible();
+        await expect(locator).toBeVisible();
 
 
 
